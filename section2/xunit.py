ab557d1dcf25f6c7463d527aa788664c407b2cd3
hyakt <hyakt0@gmail.com>
2020-03-13 01:44:37 +0900

    chapter 19

--- section2/xunit.py
+++ section2/xunit.py
@@ -4,26 +4,35 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     #  テストメソッドを動的に呼び出す
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 # テストメソッドが実行されたか記録するクラス
 class WasRun(TestCase):
-    def __init__(self, name):
+    def setUp(self):
         self.wasRun = None
-        super().__init__(name)
+        self.wasSetUp = 1
     # メソッドが実行されたか記録するメソッド
     def testMethod(self):
         self.wasRun = 1
 
+
 # テストスイートをテストするクラス
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        print('testsetup')
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
