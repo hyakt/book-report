6b701b94b1338040248321ebf0e7b4133559ad1b
hyakt <hyakt0@gmail.com>
2020-03-20 22:53:07 +0900

    chapter 21

--- section2/xunit.py
+++ section2/xunit.py
@@ -10,10 +10,13 @@ class TestCase:
         pass
     #  テストメソッドを動的に呼び出す
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
 # テストメソッドが実行されたか記録するクラス
 class WasRun(TestCase):
@@ -22,8 +25,18 @@ class WasRun(TestCase):
     # メソッドが実行されたか記録するメソッド
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def testBrokenMethod(self):
+        raise Exception
     def tearDown(self):
         self.log = self.log + "tearDown "
+
+class TestResult:
+    def __init__(self):
+        self.runCount =0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "1 run, 0 failed"
 
 # テストスイートをテストするクラス
 class TestCaseTest(TestCase):
@@ -31,5 +44,15 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
