982d53f0587daebd60523f8ebc40881a70b38a38
hyakt <hyakt0@gmail.com>
2020-04-21 20:30:04 +0900

    5.4

--- programming/learning-graphql/photo-share-api/index.js
+++ programming/learning-graphql/photo-share-api/index.js
@@ -1,3 +1,6 @@
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
@@ -7,15 +10,30 @@ const { readFileSync } = require('fs')
 const typeDefs = readFileSync('./typeDefs.graphql', 'utf-8')
 const resolvers = require('./resolvers')
 
-var app = express()
 
-const server = new ApolloServer({ typeDefs, resolvers})
+const start = async () => {
+  const app = express()
 
-server.applyMiddleware({ app })
+  const MONGO_DB = process.env.DB_HOST
 
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
-app.get('/playground', expressPlayground({ endpoint: `/graphql`}))
+  const client = await MongoClient.connect(
+    MONGO_DB,
+    { useNewUrlParser: true }
+  )
 
-app.listen({port:5000} , () => (
-  console.log(`GraphQL Server ruuning @ http://localhost:5000${server.graphqlPath}`)
-))
+  const db = client.db()
+  const context = { db }
+
+  const server = new ApolloServer({ typeDefs, resolvers, context})
+  server.applyMiddleware({ app })
+
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+  app.get('/playground', expressPlayground({ endpoint: `/graphql`}))
+
+  app.listen({ port: 5000 } , () => (
+    console.log(`GraphQL Server ruuning @ http://localhost:5000${server.graphqlPath}`)
+  ))
+
+}
+
+start()
