dc67a391092e2bc1c6a9764df75cbdd1b32098a3
hyakt <hyakt0@gmail.com>
2020-05-17 22:15:46 +0900

    6.3.4

--- programming/learning-graphql/photo-share-client/src/index.js
+++ programming/learning-graphql/photo-share-client/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
+
+const client = new ApolloClient({uri: process.env.REACT_APP_GRAPHQL_SERVER})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
